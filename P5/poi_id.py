c1437d55402a5c1158723d61b3ea65e04b967f0a
Patrick Kennedy <pmk2109@gmail.com>
2016-04-20 13:42:57 -0500

    added P6

--- P5/poi_id.py
+++ P5/poi_id.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[4]:
 
 #!/usr/bin/python
 
@@ -102,33 +102,36 @@ df = pd.DataFrame()
 df = df.from_dict(data_dict, orient='index')
 df = df.drop(['TOTAL'], axis=0)
 df = df.replace("NaN",np.nan)
-print df.count()
-print('')
+#print df.count()
+#print('')
 df = df.fillna(-1)
 
 
 
 
-labels = df['poi'].map({False:0, True:1})
-features = df.drop(['email_address', 'poi'], axis=1)
+#labels = df['poi'].map({False:0, True:1})
+features = df.drop(['email_address'], axis=1)
 
 #df = pd.DataFrame(data, columns=[features_list])
-features_clean = features[(np.abs(stats.zscore(features)) < 3).all(axis=1)]
+features_poi = features[features['poi']==1]
+features_no_poi = features[features['poi']==0]
+features_no_poi = features_no_poi.drop(['poi'], axis=1)
+
+features_clean = features_no_poi[(np.abs(stats.zscore(features_no_poi)) < 3).all(axis=1)]
 fc_names = list(features_clean.index)
-f_names = list(features.index)
+f_names = list(features_no_poi.index)
 excluded_names = set(fc_names).union(f_names) - set(fc_names).intersection(fc_names)
 
-labels = labels.ix[fc_names]
 #6 of the 33 excluded individuals were POIs (~18%)... 
 #total population of POIs in the entire dataset is 18 of 145 (~12%)...
-#this is a significant drop (33%) but let's see how well the classifiers hold up with the cleaned set
-#print df.ix[excluded_names]
+
+#print excluded_names, len(excluded_names)
+#excluded 26 outliers that are not POIs
 
 
-
-
-
-
+features_clean['poi'] = 0
+features_new = pd.concat([features_clean, features_poi], axis=0)
+#print features_new.shape, features_new.columns
 
 
 
@@ -136,17 +139,16 @@ labels = labels.ix[fc_names]
 
 
 #new features here... exclude from final model
-#features_clean['sum'] = features_clean.sum(axis=1)
-#features_clean['mean'] = features_clean.mean(axis=1)
+features_new['sum'] = features_new.sum(axis=1)
+features_new['mean'] = features_new.mean(axis=1)
 
 
 
 #put dataframe back into a dictionary so it can be pickled appropriately
-features_clean['poi'] = labels
-my_dataset = features_clean.to_dict('index')
-
-features_clean = features_clean.drop(['poi'], axis=1)
-
+my_dataset = features_new.to_dict('index')
+labels = features_new['poi']
+features_new = features_new.drop(['poi'], axis=1)
+#print features_new.shape
 
 
 ### Task 4: Try a varity of classifiers
@@ -177,52 +179,63 @@ from sklearn.cluster import KMeans
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 
+from sklearn.preprocessing import MinMaxScaler
+minmaxscaler = MinMaxScaler(feature_range=(-1,1))
 
 
-
-select = SelectKBest(k=9)
+select = SelectKBest(k=5)
 #clf_2 = RandomForestClassifier()
-#clf_1 = ExtraTreesClassifier()
-clf_2 = KNN(n_neighbors=3)
+#clf_1 = ExtraTreesClassifier(criterion='gini', n_estimators=50, min_samples_split=5, max_depth=4)
+#clf_2 = KNN(n_neighbors=9)
 #clf_1 = SVC(C=.01, kernel='sigmoid', gamma=.001)
-#clf_1 = LinearSVC(C=1., loss='squared_hinge', class_weight='balanced')
+clf_1 = LinearSVC(C=10., loss='squared_hinge', class_weight='balanced')
 #clf_1 = LogisticRegression()
-clf_1 = KMeans(n_clusters=2)
+#clf_1 = KMeans(n_clusters=2)
 
 
 
-#C_range = np.logspace(-2, 10, 13)
+C_range = np.logspace(-2, 10, 13)
 #gamma_range = np.logspace(-9, 3, 13)
 
 steps = [('feature_selection', select),
+         ('minmax', minmaxscaler),
          #('logitistic', clf_1)]
-         #('svc', clf_1)]
-         ('kmeans', clf_1),
-         #('extra_trees', clf_1),
+         ('svc', clf_1)]
+         #('kmeans', clf_1),
+         #('extra_trees', clf_1)]
          #('random_forest', clf_2)]
-         ('knn', clf_2)]
+        #('knn', clf_2)]
 
         
 
 param_grid = {"feature_selection__k":[2, 5, 9, 14, 'all'], 
               #"kmeans__n_clusters":[2,3,4,5,6,7,8],
-              #"svc__C":C_range,
-              #"svc__gamma":gamma_range,
-              #"svc__kernel":["rbf"]
-              #"knn__n_neighbors":[3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]
-              #"random_forest__n_estimators":[100, 200],
-              #"random_forest__min_samples_split":[2]
+              "svc__C":C_range,
+              "svc__loss":['hinge', 'squared_hinge'],
+              "svc__class_weight":['balanced', None]
+              #"svc__kernel":["sigmoid", "rbf"]
+              #"knn__n_neighbors":[3, 5, 7, 9]
+              #"random_forest__n_estimators":[50, 100, 200],
+              #"random_forest__min_samples_split":[2, 3],
+              #"random_forest__criterion":["gini", "entropy"],
+              #"extra_trees__n_estimators":[50, 100, 200],
+              #"extra_trees__min_samples_split":[2, 3, 5],
+              #"extra_trees__criterion":["gini", "entropy"],
+              #"extra_trees__max_depth":[4,6,8]
              }
 
 cv = StratifiedShuffleSplit(labels, n_iter=10, test_size=0.33, random_state=42)
-#clf = GridSearchCV(Pipeline(steps), param_grid, cv=3)
+#clf = GridSearchCV(Pipeline(steps), param_grid, cv=5)
 clf = Pipeline(steps)
-#clf = RandomForestClassifier()
+
 #X_train, X_test, y_train, y_test = train_test_split(features_clean, labels, 
 #                                                    test_size=0.5, 
 #                                                    random_state=42)
 
-X = features_clean.as_matrix()
+X = features_new.as_matrix()
+
+
+
 for train_index, test_index in cv:
     #print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = X[train_index], X[test_index]
@@ -233,10 +246,10 @@ for train_index, test_index in cv:
 clf.fit(X_train, y_train)
 preds = clf.predict(X_test)
 
-print "Cross validated score (cv=10): {}".format(np.mean(cross_val_score(clf, 
+print "Cross validated score (cv=5): {}".format(np.mean(cross_val_score(clf, 
                                                                         X_train, 
                                                                         y_train, 
-                                                                        cv=10)))
+                                                                        cv=5)))
 print('')
 print "Confusion Matrix: "
 print confusion_matrix(y_test, preds)
@@ -245,10 +258,13 @@ print "Precision: {}".format(precision_score(y_test, preds))
 print "Recall:    {}".format(recall_score(y_test, preds))
 
 
-#clean_data = outlierCleaner(preds, X_train, y_train)
+
+#if using gridsearch
+#clf.best_params_
 
 
-
+#if running pipeline
+#zip(features_clean.columns[select.get_support()], select.scores_)
 
 
 
@@ -259,6 +275,16 @@ print "Recall:    {}".format(recall_score(y_test, preds))
 ### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+
+
+# In[ ]:
+
+
+
+
+# In[11]:
+
+print zip(features_clean.columns[select.get_support()], select.scores_)
 
 
 # In[ ]:
